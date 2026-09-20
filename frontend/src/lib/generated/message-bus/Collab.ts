6159f7b716ebbad71be2ddb2b68fc009c2724cb2
Edouard Vanbelle <opensource@edouard.vanbelle.fr>
2026-09-20 23:41:52 +0200

    feat(collab): add new message bus types

---
+++ frontend/src/lib/generated/message-bus/Collab.ts
@@ -0,0 +1,4 @@
+import type RtSuccessResponseBody from './RtSuccessResponseBody';
+import type RtErrorResponseBody from './RtErrorResponseBody';
+type Collab = RtSuccessResponseBody | RtErrorResponseBody;
+export type { Collab as default };
